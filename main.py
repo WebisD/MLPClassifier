30077c9cdd8263c3a88c055c92f49039aac64c43
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2022-03-24 00:13:23 -0300

    updated code

--- main.py
+++ main.py
@@ -1,59 +1,83 @@
 from sklearn.datasets import load_iris
-
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-while True:
-    data = load_iris()
 
-    features =data.data
-    target = data.target
+def load_dataset():
+    resultFromFile = load_iris()
 
+    features = resultFromFile.data
+    target = resultFromFile.target
 
-    plt.figure(figsize=(16,8))
-    plt.subplot(2,2,1)
-    plt.scatter(features[:,0], features[:,1], c=target,marker='o',cmap='viridis')
-
-    Classificador = MLPClassifier(hidden_layer_sizes = (100,100,50, 160), activation="relu", alpha=1 ,max_iter=8000)
-    Classificador.fit(features,target)
-    predicao = Classificador.predict(features)
-
-    plt.subplot(2,2,3)
-    plt.scatter(features[:,0], features[:,1], c=predicao,marker='d',cmap='viridis',s=150)
-    plt.scatter(features[:,0], features[:,1], c=target,marker='o',cmap='viridis',s=15)
+    return resultFromFile, features, target
 
 
+def plot_original_data(original_features, target, subplotPosition = 1):
+    plt.figure(figsize=(16, 8))
+    plt.subplot(2, 2, subplotPosition)
+    plt.scatter(original_features[:, 0], original_features[:, 1], c=target, marker='o', cmap='viridis')
 
 
+def plot_classifier(classifier, features, target, subplotPosition):
+    classifier.fit(features, target)
+    prediction = classifier.predict(features)
 
-    pca = PCA(n_components=2, whiten=True, svd_solver='randomized')
-    pca = pca.fit(features)
-    pca_features = pca.transform(features)
-    print('Mantida %5.2f%% da informação do conjunto inicial de dados'%(sum(pca.explained_variance_ratio_)*100))
+    plt.subplot(2, 2, subplotPosition)
+    plt.scatter(features[:, 0], features[:, 1], c=prediction, marker='d', cmap='viridis', s=150)
+    plt.scatter(features[:, 0], features[:, 1], c=target, marker='o', cmap='viridis', s=15)
 
-    plt.subplot(2,2,2)
+
+def reduce_dimensions_with_cpa(original_features, target, desiredDimension, subplotPosition):
+    pca = PCA(n_components=desiredDimension, whiten=True, svd_solver='randomized')
+    pca = pca.fit(original_features)
+    pca_features = pca.transform(original_features)
+    print('Keep %5.2f%% of data from initial dataset'%(sum(pca.explained_variance_ratio_)*100))
+
+    plt.subplot(2,2,subplotPosition)
     plt.scatter(pca_features[:,0], pca_features[:,1], c=target,marker='o',cmap='viridis')
 
-
-    ClassificadorPCA = MLPClassifier(hidden_layer_sizes = (100,50,100,100), alpha=1, max_iter=7500)
-    ClassificadorPCA.fit(pca_features,target)
+    return pca_features
 
 
-    predicao = ClassificadorPCA.predict(pca_features)
+def plot_all_confusion_matrix(target, data, classifiers):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
 
-    plt.subplot(2,2,4)
-    plt.scatter(pca_features[:,0], pca_features[:,1], c=predicao,marker='d',cmap='viridis',s=150)
-    plt.scatter(pca_features[:,0], pca_features[:,1], c=target,marker='o',cmap='viridis',s=15)
+    for c, ax in zip(classifiers, axes.flatten()):
+        title, classifier, features = c
+        plot_confusion_matrix(classifier,
+                              features,
+                              target,
+                              ax=ax,
+                              display_labels=data.target_names)
+        ax.title.set_text(title)
+
+    plt.tight_layout()
+
     plt.show()
 
+def main():
+    data, original_features, target = load_dataset()
+    plot_original_data(original_features, target)
 
+    # without PCA
+    original_classifier = MLPClassifier(hidden_layer_sizes=(100, 100, 50, 160), activation="relu", alpha=1, max_iter=8000)
+    plot_classifier(original_classifier, original_features, target, subplotPosition=2)
 
-    # Sem PCA
-    plot_confusion_matrix(Classificador, features, target,include_values=True,display_labels=data.target_names)
+    # using PCA
+    pca_features = reduce_dimensions_with_cpa(original_features, target, desiredDimension=2, subplotPosition=3)
+
+    pca_classifier = MLPClassifier(hidden_layer_sizes=(100, 50, 100, 100), alpha=1, max_iter=7500)
+    plot_classifier(pca_classifier, pca_features, target, subplotPosition=4)
+
+    # graphs plot
     plt.show()
 
-    # Com PCA
-    plot_confusion_matrix(ClassificadorPCA, pca_features, target,include_values=True,display_labels=data.target_names)
-    plt.show()
+    # confusion matrix plot
+    plot_all_confusion_matrix(target, data, [
+        ("Without PCA", original_classifier, original_features),
+        ("With PCA", pca_classifier, pca_features)
+    ])
+
+main()
